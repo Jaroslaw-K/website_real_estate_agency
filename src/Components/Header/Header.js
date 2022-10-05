35cb930f181bce1b1c5192c81c5ebf378e0017e3
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-10-05 13:42:43 +0200

    created Header component, currently for Mobile

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,38 @@
+import "./Header.scss";
+import { useState } from "react";
+import Logo from "../../assets/logo.svg";
+
+import NavigationButton from "./NavigationButton/NavigationButton";
+import LanguagesButton from "./LanguagesButton/LanguagesButton";
+import DarkModeButton from "./DarkModeButton/DarkModeButton";
+import LanguagesList from "./LanguagesList/LanguagesList";
+import WindowNavigationMobile from "./WIndowNavigationMobile/WindowNavigatonMobile";
+
+
+const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+  const [showLanguagesList, setShowLanguagesList] = useState(false);
+
+  const toggleNavHandler = () => {
+    setShowNav((previousState) => !previousState);
+  };
+  const toggleLanguageList = () => {
+    setShowLanguagesList((previousState) => !previousState);
+  }
+  return (
+    <header className="headerMobile">
+      <img src={Logo} alt="logo" className="headerMobile__logo" />
+      <nav className="headerMobile__navContainer">
+        <div className="navContainer__optionsContainer">
+         <DarkModeButton/>
+         <LanguagesButton onToggleLanguageList={toggleLanguageList}/>
+         <LanguagesList showLanguagesList={showLanguagesList} onToggleLanguageList={toggleLanguageList}/>
+        </div>
+        <NavigationButton onToggleNavHandler={toggleNavHandler} showNav={showNav}/>
+      </nav>
+    <WindowNavigationMobile/>
+    </header>
+  );
+};
+
+export default Header;
