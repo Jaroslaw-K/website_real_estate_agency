cdcb69180bb88b6a59352e94056a560d3f11a3e3
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-10-06 20:05:43 +0200

    created desktop functionality

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,9 @@
 import "./Header.scss";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Logo from "../../assets/logo.svg";
+import { useMediaQuery } from "react-responsive";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import NavigationButton from "./NavigationButton/NavigationButton";
 import LanguagesButton from "./LanguagesButton/LanguagesButton";
@@ -11,26 +14,78 @@ import WindowNavigationMobile from "./WindowNavigationMobile/WindowNavigationMob
 const Header = () => {
   const [showNav, setShowNav] = useState(false);
   const [showLanguagesList, setShowLanguagesList] = useState(false);
+  const darkMode = useSelector((state) => state.darkMode);
+  const languagePrimary = useSelector((state) => state.languagePrimary);
 
   const toggleNavHandler = () => {
     setShowNav((previousState) => !previousState);
   };
   const toggleLanguageList = () => {
     setShowLanguagesList((previousState) => !previousState);
-  }
+  };
   return (
-    <header className="headerMobile">
-      <img src={Logo} alt="logo" className="headerMobile__logo" />
-      <nav className="headerMobile__navContainer">
-        <div className="navContainer__optionsContainer">
-         <DarkModeButton/>
-         <LanguagesButton onToggleLanguageList={toggleLanguageList}/>
-         <LanguagesList showLanguagesList={showLanguagesList} onToggleLanguageList={toggleLanguageList}/>
-        </div>
-        <NavigationButton onToggleNavHandler={toggleNavHandler} showNav={showNav}/>
-      </nav>
-    <WindowNavigationMobile showNav={showNav}/>
-    </header>
+    <Fragment>
+      {useMediaQuery({ maxWidth: 1200 }) && (
+        <header className="headerMobile">
+          <img src={Logo} alt="logo" className="headerMobile__logo" />
+          <nav className="headerMobile__navContainer">
+            <div className="navContainer__optionsContainer">
+              <DarkModeButton />
+              <LanguagesButton onToggleLanguageList={toggleLanguageList} />
+              <LanguagesList showLanguagesList={showLanguagesList} onToggleLanguageList={toggleLanguageList} />
+            </div>
+            <NavigationButton onToggleNavHandler={toggleNavHandler} showNav={showNav} />
+          </nav>
+          <WindowNavigationMobile showNav={showNav} onToggleNavHandler={toggleNavHandler} />
+        </header>
+      )}
+      {!useMediaQuery({ maxWidth: 1200 }) && (
+        <header className="headerDesktop">
+          <div className="headerDesktop__containerTop">
+            <div className="containerTop__left">
+              <div className="left__emailContainer">
+                <i className="bi bi-envelope-fill"></i>&nbsp;&nbsp;Email Address
+              </div>
+              <div className="left__phoneContainer">
+                <i className="bi bi-telephone-forward-fill"></i>&nbsp;&nbsp;Phone Number
+              </div>
+              <div className="left__line left__line--1"></div>
+              <div className="left__line left__line--2"></div>
+              <div className="left__line left__line--3"></div>
+            </div>
+            <div className="containerTop__central"></div>
+            <div className={darkMode ? "containerTop__right containerTop__right--dark" : "containerTop__right containerTop__right--bright"}>
+              <div className="right__line right__line--1"></div>
+              <div className="right__line right__line--2"></div>
+              <div className="right__line right__line--3"></div>
+              <div className={darkMode ? "right__backgroundBox right__backgroundBox--dark" : "right__backgroundBox right__backgroundBox--bright"}></div>
+              <div className="right__optionsContainer">
+                <DarkModeButton />
+                <LanguagesButton onToggleLanguageList={toggleLanguageList} />
+                <LanguagesList showLanguagesList={showLanguagesList} onToggleLanguageList={toggleLanguageList} />
+              </div>
+            </div>
+          </div>
+          <div className={darkMode ? "headerDesktop__containerBottom headerDesktop__containerBottom--dark" : "headerDesktop__containerBottom headerDesktop__containerBottom--bright"}>
+            <NavLink to="" className={darkMode ? "containerBottom__link containerBottom__link--dark" : "containerBottom__link containerBottom__link--bright"}>
+              {languagePrimary ? "OFERTA" : "OFFER"}
+            </NavLink>
+            <NavLink to="/sell-or-rent" className={darkMode ? "containerBottom__link containerBottom__link--dark" : "containerBottom__link containerBottom__link--bright"}>
+              {languagePrimary ? "SPRZEDAŻ/WYNAJEM" : "SELL/RENT"}
+            </NavLink>
+            <NavLink to="/about-us" className={darkMode ? "containerBottom__link containerBottom__link--dark" : "containerBottom__link containerBottom__link--bright"}>
+              {languagePrimary ? "O NAS" : "ABOUT US"}
+            </NavLink>
+            <NavLink to="/privacy-policy" className={darkMode ? "containerBottom__link containerBottom__link--dark" : "containerBottom__link containerBottom__link--bright"}>
+              {languagePrimary ? "POLITYKA RODO" : "PRIVACY POLICY"}
+            </NavLink>
+            <NavLink to="/contact" className={darkMode ? "containerBottom__link containerBottom__link--dark" : "containerBottom__link containerBottom__link--bright"}>
+              {languagePrimary ? "KONTAKT" : "CONTACT"}
+            </NavLink>
+          </div>
+        </header>
+      )}
+    </Fragment>
   );
 };
 
