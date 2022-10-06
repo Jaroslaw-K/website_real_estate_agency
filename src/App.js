4822a95ce4e49d6c59f64a1e57da26cdc298cad0
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-10-06 19:48:32 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./Components/Header/Header";
-
+import AboutUs from "./Pages/AboutUs";
+import Contact from "./Pages/Contact";
 import Offer from "./Pages/Offer";
+import PrivacyPolicy from "./Pages/PrivacyPolicy";
+import SellRent from "./Pages/SellRent";
 
 const App = () => {
   return (
@@ -11,6 +14,10 @@ const App = () => {
       <Routes>
         <Route path="/*" element={<Navigate to="/offer" />} />
         <Route path="/offer" element={<Offer />} />
+        <Route path="/sell-or-rent" element={<SellRent />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Fragment>
   );
