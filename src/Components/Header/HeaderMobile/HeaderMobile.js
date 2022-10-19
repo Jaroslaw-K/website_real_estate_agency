f5f170718ac7e8253a45f37a98bac2b5f75196ad
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-10-19 17:25:10 +0200

    removed redundant code

--- src/Components/Header/HeaderMobile/HeaderMobile.js
+++ src/Components/Header/HeaderMobile/HeaderMobile.js
@@ -9,17 +9,17 @@ import WindowNavigationMobile from "./WindowNavigationMobile/WindowNavigationMob
 const HeaderMobile = (props) => {
   return (
     <header className="headerMobile">
-          <img src={Logo} alt="logo" className="headerMobile__logo" />
-          <nav className="headerMobile__navContainer">
-            <div className="navContainer__optionsContainer">
-              <DarkModeButton />
-              <LanguagesButton onToggleLanguagesList={props.onToggleLanguagesList} />
-              <LanguagesList showLanguagesList={props.showLanguagesList} onToggleLanguagesList={props.onToggleLanguagesList} />
-            </div>
-            <NavigationButton onToggleNavHandler={props.onToggleNavHandler} showNav={props.showNav} />
-          </nav>
-          <WindowNavigationMobile showNav={props.showNav} onToggleNavHandler={props.onToggleNavHandler} />
-        </header>
+      <img src={Logo} alt="logo" className="headerMobile__logo" />
+      <nav className="headerMobile__navContainer">
+        <div className="navContainer__optionsContainer">
+          <DarkModeButton />
+          <LanguagesButton onToggleLanguagesList={props.onToggleLanguagesList} />
+          <LanguagesList showLanguagesList={props.showLanguagesList} onToggleLanguagesList={props.onToggleLanguagesList} />
+        </div>
+        <NavigationButton onToggleNavHandler={props.onToggleNavHandler} showNav={props.showNav} />
+      </nav>
+      <WindowNavigationMobile showNav={props.showNav} onToggleNavHandler={props.onToggleNavHandler} />
+    </header>
   );
 };
 
