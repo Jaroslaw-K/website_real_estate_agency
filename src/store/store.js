8ddbcb665d604b54d65cadb80243b6a77aa8671e
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-10-24 19:10:18 +0200

    corrected reducer name, updated state, added cursor display

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,36 @@
 import { createStore } from "redux";
 
-const initialState = { darkMode: localStorage.getItem("darkMode") === "true", languagePrimary: localStorage.getItem("languagePrimary") === "true" };
+const initialState = { darkMode: localStorage.getItem("darkMode") === "true", 
+                      languagePrimary: localStorage.getItem("languagePrimary") === "true",
+                      changedMouse: false 
+                     };
 
-const darkModeReducer = (state = initialState, action) => {
+const websiteReducer = (state = initialState, action) => {
   if (action.type === "switchMode") {
     return {
       darkMode: !state.darkMode,
       languagePrimary: state.languagePrimary,
+      changedMouse: state.changedMouse 
     };
   }
   if (action.type === "switchLanguage") {
     return {
       darkMode: state.darkMode,
       languagePrimary: action.languagePrimary,
+      changedMouse: state.changedMouse 
+    };
+  }
+  if (action.type === "switchMouse") {
+    return {
+      darkMode: state.darkMode,
+      languagePrimary: state.languagePrimary,
+      changedMouse: !state.changedMouse
     };
   }
   return state;
 };
 
-const store = createStore(darkModeReducer);
+const store = createStore(websiteReducer);
 
 const storeSubscribe = () => {
   localStorage.setItem("darkMode", store.getState().darkMode);
