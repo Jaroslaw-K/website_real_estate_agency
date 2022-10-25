73f8494da86142268fe9cd3a013dce9d6967ae2c
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-10-25 19:33:45 +0200

    removed cursor functionality

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,6 @@ import { createStore } from "redux";
 
 const initialState = { darkMode: localStorage.getItem("darkMode") === "true", 
                       languagePrimary: localStorage.getItem("languagePrimary") === "true",
-                      changedMouse: false 
                      };
 
 const websiteReducer = (state = initialState, action) => {
@@ -10,21 +9,12 @@ const websiteReducer = (state = initialState, action) => {
     return {
       darkMode: !state.darkMode,
       languagePrimary: state.languagePrimary,
-      changedMouse: state.changedMouse 
     };
   }
   if (action.type === "switchLanguage") {
     return {
       darkMode: state.darkMode,
       languagePrimary: action.languagePrimary,
-      changedMouse: state.changedMouse 
-    };
-  }
-  if (action.type === "switchMouse") {
-    return {
-      darkMode: state.darkMode,
-      languagePrimary: state.languagePrimary,
-      changedMouse: !state.changedMouse
     };
   }
   return state;
