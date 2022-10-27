80d86227282fe34140ce50a5f20a35c88778891b
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-10-27 16:38:24 +0200

    added small window with slides

--- src/Components/Slider/SliderDesktop/SliderDesktop.js
+++ src/Components/Slider/SliderDesktop/SliderDesktop.js
@@ -1,19 +1,56 @@
 import "./SliderDesktop.scss";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState, Fragment } from "react";
 import { useSelector } from "react-redux";
+
+import image1 from "../../../assets/imagesSlider/259588.jpg";
+import image2 from "../../../assets/imagesSlider/280222.jpg";
+import image3 from "../../../assets/imagesSlider/209296.jpg";
+import image4 from "../../../assets/imagesSlider/1438834.jpg";
+import image5 from "../../../assets/imagesSlider/5502227.jpg";
+import image6 from "../../../assets/imagesSlider/5524205.jpg";
+import image7 from "../../../assets/imagesSlider/7031408.jpg";
 
 const SliderDesktop = () => {
   const darkMode = useSelector((state) => state.darkMode);
   const slidesContainer = useRef();
   const containerButtonLeft = useRef();
   const containerButtonRight = useRef();
+  const [currentId, setCurrentId] = useState(undefined);
+  const [slides, setSlides] = useState([
+    {
+      id: 0,
+      url: image1,
+    },
+    {
+      id: 1,
+      url: image2,
+    },
+    {
+      id: 2,
+      url: image3,
+    },
+    {
+      id: 3,
+      url: image4,
+    },
+    {
+      id: 4,
+      url: image5,
+    },
+    {
+      id: 5,
+      url: image6,
+    },
+    {
+      id: 6,
+      url: image7,
+    },
+  ]);
 
   useEffect(() => {
-    for (let i = 0; i < slidesContainer.current.childNodes.length; i++) {
-      if (slidesContainer.current.childNodes[i].classList.contains("current")) {
-        slidesContainer.current.childNodes[i].style.display = "block";
-      }
-    }
+    slidesContainer.current.childNodes[0].classList.add("current");
+    slidesContainer.current.childNodes[0].style.display = "block";
+    setCurrentId(0);
   }, []);
 
   const toggleLeftImgHandler = () => {
@@ -51,9 +88,11 @@ const SliderDesktop = () => {
             if (slidesContainer.current.childNodes[i - 1] !== undefined) {
               slidesContainer.current.childNodes[i - 1].classList.add("current");
               slidesContainer.current.childNodes[i - 1].classList.remove("second");
+              setCurrentId(i - 1);
             } else {
               slidesContainer.current.childNodes[slidesContainer.current.childNodes.length - 1].classList.add("current");
               slidesContainer.current.childNodes[slidesContainer.current.childNodes.length - 1].classList.remove("second");
+              setCurrentId(slidesContainer.current.childNodes.length - 1);
             }
           }
         }, 1);
@@ -87,7 +126,6 @@ const SliderDesktop = () => {
           } else {
             slidesContainer.current.childNodes[0].style.left = `${counterSecond}%`;
           }
-
           if (counterCurrent === 100 || counterSecond === 0) {
             containerButtonLeft.current.style.pointerEvents = "auto";
             containerButtonRight.current.style.pointerEvents = "auto";
@@ -97,9 +135,11 @@ const SliderDesktop = () => {
             if (slidesContainer.current.childNodes[i + 1] !== undefined) {
               slidesContainer.current.childNodes[i + 1].classList.add("current");
               slidesContainer.current.childNodes[i + 1].classList.remove("second");
+              setCurrentId(i + 1);
             } else {
               slidesContainer.current.childNodes[0].classList.add("current");
               slidesContainer.current.childNodes[0].classList.remove("second");
+              setCurrentId(0);
             }
           }
         }, 1);
@@ -107,27 +147,52 @@ const SliderDesktop = () => {
     }
   };
 
+  const toggleSmallLeftImgHandler = () => {
+    console.log('moveLeft');
+  }
+
+  const toggleSmallRightImgHandler = () => {
+    console.log('moveRight');
+  }
+ 
+  let slidesMain = slides.map((slide) => <div key={slide.id} className="slidesContainer__slide" style={{ backgroundImage: `url(${slide.url})` }}></div>);
+  let slidesSmall = <Fragment>
+       {slides.map((slide) => { if (slide.id === currentId && slide.id === 0) {
+        return <div className="slidesContainer__slide" style={{ backgroundImage: `url(${slides[slides.length - 2].url})` }}></div>
+        } else if (slide.id === currentId && slide.id === 1) {
+          return <div className="slidesContainer__slide" style={{ backgroundImage: `url(${slides[slides.length - 1].url})` }}></div>
+        }  else if (slide.id === currentId && slide.id >= 2) {
+          return <div className="slidesContainer__slide" style={{ backgroundImage: `url(${slides[slide.id - 2].url})` }}></div>
+        }
+        })}
+        
+      {slides.map((slide) => { if (slide.id === currentId) {return <div className="slidesContainer__slide" style={{ backgroundImage: `url(${slides[slide.id <= 0? slides.length - 1:slide.id - 1].url})` }}></div>}})}
+      {slides.map((slide) => { if (slide.id === currentId) {return <div className="slidesContainer__slide currentSmall" style={{ backgroundImage: `url(${slides[slide.id].url})` }}></div>}})}
+      {slides.map((slide) => { if (slide.id === currentId) {return <div className="slidesContainer__slide" style={{ backgroundImage: `url(${slides[slide.id >= slides.length - 1? 0:slide.id + 1].url})` }}></div>}})}
+      
+      {slides.map((slide) => { if (slide.id === currentId && slide.id === slides.length - 2) {
+        return <div className="slidesContainer__slide" style={{ backgroundImage: `url(${slides[0].url})` }}></div>
+        } else if (slide.id === currentId && slide.id === slides.length - 1) {
+          return <div className="slidesContainer__slide" style={{ backgroundImage: `url(${slides[1].url})` }}></div>
+        }  else if (slide.id === currentId && slide.id >= 0) {
+          return <div className="slidesContainer__slide" style={{ backgroundImage: `url(${slides[slide.id + 2].url})` }}></div>
+        }
+        })}
+   </Fragment>
+
   return (
     <div className={darkMode ? "sliderDesktop sliderDesktop--dark" : "sliderDesktop sliderDesktop--bright"}>
       <div ref={containerButtonLeft} className="sliderDesktop__containerButton sliderDesktop__containerButton--left" onClick={toggleLeftImgHandler}></div>
       <div ref={containerButtonRight} className="sliderDesktop__containerButton sliderDesktop__containerButton--right" onClick={toggleRightImgHandler}></div>
       <div ref={slidesContainer} className="sliderDesktop__slidesContainer">
-        <div className="slidesContainer__slide current"></div>
-        <div className="slidesContainer__slide"></div>
-        <div className="slidesContainer__slide"></div>
-        <div className="slidesContainer__slide"></div>
-        <div className="slidesContainer__slide"></div>
+        {slidesMain}
       </div>
       <div className="sliderDesktop__smallSlidesContainer">
-        <i class="bi bi-caret-left-fill smallSlidesContainer__arrow smallSlidesContainer__arrow--left"></i>
-        <i class="bi bi-caret-right-fill smallSlidesContainer__arrow smallSlidesContainer__arrow--right"></i>
+        <i className="bi bi-caret-left-fill smallSlidesContainer__arrow smallSlidesContainer__arrow--left" onClick={toggleSmallLeftImgHandler}></i>
+        <i className="bi bi-caret-right-fill smallSlidesContainer__arrow smallSlidesContainer__arrow--right" onClick={toggleSmallRightImgHandler}></i>
         <div className="smallSlidesContainer__slidesContainer">
-            <div className="slidesContainer__slide"></div>
-            <div className="slidesContainer__slide"></div>
-            <div className="slidesContainer__slide"></div>
-            <div className="slidesContainer__slide"></div>
-            <div className="slidesContainer__slide"></div>
-        </div>
+        {slidesSmall}
+          </div>
       </div>
     </div>
   );
