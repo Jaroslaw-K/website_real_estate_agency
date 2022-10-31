81642c8164ed113faa346873a8dfeaa28f499e2e
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-10-31 17:55:06 +0100

    removed redundant code

--- src/Components/Header/LanguagesButton/LanguagesButton.js
+++ src/Components/Header/LanguagesButton/LanguagesButton.js
@@ -5,9 +5,6 @@ const LanguagesButton = (props) => {
   const darkMode = useSelector((state) => state.darkMode);
   const languagePrimary = useSelector((state) => state.languagePrimary);
   return (
-    // <button className={darkMode ? "languageButton languageButton--dark" : "languageButton languageButton--bright"} onClick={props.onToggleLanguagesList}>
-    //   <span className={darkMode ? "languageButton__countryTxt languageButton__countryTxt--dark" : "languageButton__countryTxt languageButton__countryTxt--bright"}>{languagePrimary ? "PL" : "EN"}</span>
-    // </button>
     <button className={darkMode ? "languageButton languageButton--dark" : "languageButton languageButton--bright"} onClick={props.onToggleLanguagesList}>
       <span className={darkMode ? "languageButton__countryText" : "languageButton__countryText"}>{languagePrimary ? "PL" : "EN"}</span>
     </button>
