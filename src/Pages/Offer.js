cec35f3a3a08211447e40d4f767a1c71bd26ffbe
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-11-04 18:13:02 +0100

    removed redundant code

--- src/Pages/Offer.js
+++ src/Pages/Offer.js
@@ -1,13 +1,52 @@
 import "./Offer.scss";
 import Slider from "../Components/Slider/Slider";
 
+import image1 from "../assets/imagesSlider/7031408.jpg";
+import image2 from "../assets/imagesSlider/5502227.jpg";
+import image3 from "../assets/imagesSlider/5524205.jpg";
+import image4 from "../assets/imagesSlider/280222.jpg";
+
 const Offer = (props) => {
+  const slidesArray = [
+    {
+      id: 0,
+      url: image1,
+      text1_EN: "PLACE FOR TEXT",
+      text2_EN: "PLACE FOR TEXT",
+      text1_PL: "MIEJSCE NA TEKST",
+      text2_PL: "MIEJSCE NA TEKST",
+    },
+    {
+      id: 1,
+      url: image2,
+      text1_EN: "PLACE FOR TEXT",
+      text2_EN: "PLACE FOR TEXT",
+      text1_PL: "MIEJSCE NA TEKST",
+      text2_PL: "MIEJSCE NA TEKST",
+    },
+    {
+      id: 2,
+      url: image3,
+      text1_EN: "PLACE FOR TEXT",
+      text2_EN: "PLACE FOR TEXT",
+      text1_PL: "MIEJSCE NA TEKST",
+      text2_PL: "MIEJSCE NA TEKST",
+    },
+    {
+      id: 3,
+      url: image4,
+      text1_EN: "PLACE FOR TEXT",
+      text2_EN: "PLACE FOR TEXT",
+      text1_PL: "MIEJSCE NA TEKST",
+      text2_PL: "MIEJSCE NA TEKST",
+    },
+  ];
   const moveSlider = (moveSlider) => {
     moveSlider();
-  }
+  };
   return (
     <main className="offer">
-      <Slider moveSlider={moveSlider}/>
+      <Slider moveSlider={moveSlider} slidesArray={slidesArray} />
     </main>
   );
 };
