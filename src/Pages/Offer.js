b26ecf94c9d2c50f29c7416141ebec22bc4b5940
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-11-21 18:09:23 +0100

    added SearchEngine component

--- src/Pages/Offer.js
+++ src/Pages/Offer.js
@@ -1,52 +1,25 @@
 import "./Offer.scss";
+import { useSelector } from "react-redux";
+import { useMediaQuery } from "react-responsive";
 import Slider from "../Components/Slider/Slider";
+import { autoMoveSliderData } from "../data/data";
+import SearchEngine from "../Components/SearchEngine/SearchEngine";
 
-import image1 from "../assets/imagesSlider/7031408.jpg";
-import image2 from "../assets/imagesSlider/5502227.jpg";
-import image3 from "../assets/imagesSlider/5524205.jpg";
-import image4 from "../assets/imagesSlider/280222.jpg";
-
-const Offer = (props) => {
-  const slidesArray = [
-    {
-      id: 0,
-      url: image1,
-      text1_EN: "PLACE FOR TEXT",
-      text2_EN: "PLACE FOR TEXT",
-      text1_PL: "MIEJSCE NA TEKST",
-      text2_PL: "MIEJSCE NA TEKST",
-    },
-    {
-      id: 1,
-      url: image2,
-      text1_EN: "PLACE FOR TEXT",
-      text2_EN: "PLACE FOR TEXT",
-      text1_PL: "MIEJSCE NA TEKST",
-      text2_PL: "MIEJSCE NA TEKST",
-    },
-    {
-      id: 2,
-      url: image3,
-      text1_EN: "PLACE FOR TEXT",
-      text2_EN: "PLACE FOR TEXT",
-      text1_PL: "MIEJSCE NA TEKST",
-      text2_PL: "MIEJSCE NA TEKST",
-    },
-    {
-      id: 3,
-      url: image4,
-      text1_EN: "PLACE FOR TEXT",
-      text2_EN: "PLACE FOR TEXT",
-      text1_PL: "MIEJSCE NA TEKST",
-      text2_PL: "MIEJSCE NA TEKST",
-    },
-  ];
+const Offer = () => {
+  const darkMode = useSelector((state) => state.darkMode);
+  const languagePrimary = useSelector((state) => state.languagePrimary);
   const moveSlider = (moveSlider) => {
     moveSlider();
   };
   return (
-    <main className="offer">
-      <Slider moveSlider={moveSlider} slidesArray={slidesArray} />
+    <main className={darkMode ? "offer offer--dark" : "offer offer--bright"}>
+      <Slider moveSlider={moveSlider} slidesArray={autoMoveSliderData} />
+      <div className="offer__searchBackground">
+        <div className="searchBackground__background">
+          <SearchEngine />
+          {useMediaQuery({ maxWidth: 1200 }) && <div className="background__info">{languagePrimary? "CO ROBIMY?":"WHAT WE DO?"}</div>}
+        </div>
+      </div>
     </main>
   );
 };
