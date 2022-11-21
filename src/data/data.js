1efcbec70cc0a6c13d722e39f9f7612f5ef622c5
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2022-11-21 18:13:26 +0100

    created file with data

---
+++ src/data/data.js
@@ -0,0 +1,77 @@
+import image1 from "../assets/imagesSlider/7031408.jpg";
+import image2 from "../assets/imagesSlider/5502227.jpg";
+import image3 from "../assets/imagesSlider/5524205.jpg";
+import image4 from "../assets/imagesSlider/280222.jpg";
+
+const offers = [
+  {
+    id: 0,
+    offerNumber: "",
+    propertyTypePL: "",
+    propertyTypeEN: "",
+    transactionTypePL: "",
+    transactionTypeEN: "",
+    marketTypePL: "",
+    marketTypeEN: "",
+    provincePL: "",
+    provinceEN: "",
+    place: "",
+    district: "",
+    price: "",
+    roomsNumber: "",
+    floorNumber: "",
+    numberOfFloors: "",
+    livingArea: "",
+    buildYear: "",
+    descriptionPL1: "",
+    descriptionEN1: "",
+
+    agentName: "",
+    agentSurname: "",
+    agentPhoneNumber: "",
+    agentEmailAddress: "",
+  },
+];
+
+const offersImages = [
+  {
+    id: 0,
+    url: image1,
+  },
+];
+
+const autoMoveSliderData = [
+  {
+    id: 0,
+    url: image1,
+    text1_EN: "PLACE FOR TEXT",
+    text2_EN: "PLACE FOR TEXT",
+    text1_PL: "MIEJSCE NA TEKST",
+    text2_PL: "MIEJSCE NA TEKST",
+  },
+  {
+    id: 1,
+    url: image2,
+    text1_EN: "PLACE FOR TEXT",
+    text2_EN: "PLACE FOR TEXT",
+    text1_PL: "MIEJSCE NA TEKST",
+    text2_PL: "MIEJSCE NA TEKST",
+  },
+  {
+    id: 2,
+    url: image3,
+    text1_EN: "PLACE FOR TEXT",
+    text2_EN: "PLACE FOR TEXT",
+    text1_PL: "MIEJSCE NA TEKST",
+    text2_PL: "MIEJSCE NA TEKST",
+  },
+  {
+    id: 3,
+    url: image4,
+    text1_EN: "PLACE FOR TEXT",
+    text2_EN: "PLACE FOR TEXT",
+    text1_PL: "MIEJSCE NA TEKST",
+    text2_PL: "MIEJSCE NA TEKST",
+  },
+];
+export { offers, offersImages,  autoMoveSliderData};
