385db10b7ad770073ae9abab7c9abcb759c643eb
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2023-01-03 18:18:16 +0100

    added comments

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,17 @@
 import { createStore } from "redux";
 
-const initialState = { darkMode: localStorage.getItem("darkMode") === "true", 
-                      languagePrimary: localStorage.getItem("languagePrimary") === "true",
+const initialState = { darkMode: localStorage.getItem("darkMode") === "true", // WHEN PAGE IS INITALLY RENDER, WILL RETURN FALSE
+                      languagePrimary: localStorage.getItem("languagePrimary") === "true", // WHEN PAGE IS INITALLY RENDER, WILL RETURN FALSE
                      };
 
 const websiteReducer = (state = initialState, action) => {
-  if (action.type === "switchMode") {
+  if (action.type === "switchMode") { // RESPONSIBLE FOR WEBSITE COLORS - DARK OR BRIGHT WEBSITE, BY DEFAULT IS BRIGHT
     return {
       darkMode: !state.darkMode,
       languagePrimary: state.languagePrimary,
     };
   }
-  if (action.type === "switchLanguage") {
+  if (action.type === "switchLanguage") { // RESPONSIBLE FOR WEBSITE LANGUAGE - ENGLISH OR POLISH, BY DEFAULT IS SET TO ENGLISH (languagePrimary is false)
     return {
       darkMode: state.darkMode,
       languagePrimary: action.languagePrimary,
