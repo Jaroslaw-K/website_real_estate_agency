d931e6599469c1c9b6e316543ebee1910aea30f5
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2023-01-03 18:29:50 +0100

    added comments

--- src/Components/Cursor/Cursor.js
+++ src/Components/Cursor/Cursor.js
@@ -4,28 +4,29 @@ import { useSelector } from "react-redux";
 import { BrowserView, isBrowser } from "react-device-detect";
 
 const Cursor = () => {
-  const darkMode = useSelector((state) => state.darkMode);
+  const darkMode = useSelector((state) => state.darkMode); // RESPONSIBLE FOR WEBSITE COLORS - DARK OR BRIGHT WEBSITE, BY DEFAULT IS BRIGHT
   const cursor = useRef(null);
 
   useEffect(() => {
-
     if (isBrowser) {
-    document.addEventListener("mousemove", (event) => {
-      const { x, y } = event;
-      const countX = x - cursor.current.clientWidth;
-      const countY = y - cursor.current.clientHeight;
-      cursor.current.style.transform = `translate3d(${countX}px, ${countY}px, 0)`;
+      document.addEventListener("mousemove", (event) => {
+        const { x, y } = event;
+        const countX = x - cursor.current.clientWidth;
+        const countY = y - cursor.current.clientHeight;
+        cursor.current.style.transform = `translate3d(${countX}px, ${countY}px, 0)`;
 
-      if (y <= 1 && x <= 0) {
-        cursor.current.style.display = "none";
-      } else if (y <= 1 && x > 1) {
-        cursor.current.style.display = "none";
-      } else if (y > 2 && x <= 0) {
-        cursor.current.style.display = "none";
-      } else {
-        cursor.current.style.display = "block";
-      }
-    })};
+        // RESPONSIBLE FOR REMOVE CURSOR WHEN USER MOVE MOUSE OUTSIDE PAGE VIEW
+        if (y <= 1 && x <= 0) {
+          cursor.current.style.display = "none";
+        } else if (y <= 1 && x > 1) {
+          cursor.current.style.display = "none";
+        } else if (y > 2 && x <= 0) {
+          cursor.current.style.display = "none";
+        } else {
+          cursor.current.style.display = "block";
+        }
+      });
+    }
   }, []);
 
   return (
