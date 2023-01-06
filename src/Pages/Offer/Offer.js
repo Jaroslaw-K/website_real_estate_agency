50eb0a125ff8bdbaa48eba4a510ebdfe0c3ac738
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2023-01-06 18:44:50 +0100

    added part of website with company specialisations

--- src/Pages/Offer/Offer.js
+++ src/Pages/Offer/Offer.js
@@ -6,10 +6,15 @@ import { autoMoveSliderData } from "../../data/data";
 // COMPONENTS
 import Slider from "../../Components/Slider/Slider";
 import SearchEngine from "./SearchEngine/SearchEngine";
+import Specialsation from "./Specialisation/Specialisation";
+// IMAGES
+import image1 from "../../assets/images/specialisations/8112172.jpg";
+import image2 from "../../assets/images/specialisations/7176319.jpg";
+import image3 from "../../assets/images/specialisations/261621.jpg";
 
 const Offer = () => {
   const darkMode = useSelector((state) => state.darkMode); // RESPONSIBLE FOR WEBSITE COLORS - DARK OR BRIGHT WEBSITE, BY DEFAULT IS BRIGHT
-  const languagePrimary = useSelector((state) => state.languagePrimary);  // RESPONSIBLE FOR WEBSITE LANGUAGE - ENGLISH OR POLISH
+  const languagePrimary = useSelector((state) => state.languagePrimary); // RESPONSIBLE FOR WEBSITE LANGUAGE - ENGLISH OR POLISH
   const moveSlider = (moveSlider) => {
     moveSlider(); // FUNCTION RESPONSIBLE FOR AUTO-MOVE SLIDER
   };
@@ -20,20 +25,25 @@ const Offer = () => {
         <div className="searchBackground__background">
           <SearchEngine />
           {useMediaQuery({ maxWidth: 1200 }) && <div className="background__info background__info--mobile">{languagePrimary ? "CO ROBIMY?" : "WHAT WE DO?"}</div>}
-          {!useMediaQuery({ maxWidth: 1200 }) && <div className="background__info background__info--desktop">
-            <div className="info__txt">{languagePrimary ? "CO ROBIMY?" : "WHAT WE DO?"}</div>
-            <div className="info__container">
-              <div className="container__specialisation">
-                <div className="specialisation__containerTop"></div>
-                <div className="specialisation__containerBottom"></div>
+          {!useMediaQuery({ maxWidth: 1200 }) && (
+            <div className="background__info background__info--desktop">
+              <div className="info__txt">{languagePrimary ? "CO ROBIMY?" : "WHAT WE DO?"}</div>
+              <div className="info__container">
+                <Specialsation image={image1} title={languagePrimary ? "SPRZEDAŻ/WYNAJEM" : "SELL/RENT"} text={languagePrimary ? "SPRZEDAJEMY ORAZ WYNAJMUJEMY NIERUCHOMOŚCI" : "WE SELL AND RENT PROPERTIES"} />
+                <Specialsation image={image2} title={languagePrimary ? "DORADZTWO" : "ADVISORY"} text={languagePrimary ? "DORADZAMY W SPRAWACH KREDYTÓW" : "WE ADVISE ON LOAN MATTERS"} />
+                <Specialsation image={image3} title={languagePrimary ? "WSPÓŁPRACA" : "COOPERATION"} text={languagePrimary ? "WSPÓŁPRACUJEMY Z BANKAMI ORAZ INNYMI AGENCJAMI NIERUCHOMOŚCI" : "WE COOPERATE WITH BANKS AND OTHER REAL ESTATE AGENCIES"} />
               </div>
-              <div className="container__specialisation">1</div>
-              <div className="container__specialisation">1</div>
             </div>
-            </div>}
+          )}
         </div>
       </div>
-     
+      {useMediaQuery({ maxWidth: 1200 }) && (
+        <div className="offer__specialisations">
+          <Specialsation image={image1} title={languagePrimary ? "SPRZEDAŻ/WYNAJEM" : "SELL/RENT"} text={languagePrimary ? "SPRZEDAJEMY ORAZ WYNAJMUJEMY NIERUCHOMOŚCI" : "WE SELL AND RENT PROPERTIES"} />
+          <Specialsation image={image2} title={languagePrimary ? "DORADZTWO" : "ADVISORY"} text={languagePrimary ? "DORADZAMY W SPRAWACH KREDYTÓW" : "WE ADVISE ON LOAN MATTERS"} />
+          <Specialsation image={image3} title={languagePrimary ? "WSPÓŁPRACA" : "COOPERATION"} text={languagePrimary ? "WSPÓŁPRACUJEMY Z BANKAMI ORAZ INNYMI AGENCJAMI NIERUCHOMOŚCI" : "WE COOPERATE WITH BANKS AND OTHER REAL ESTATE AGENCIES"} />
+        </div>
+      )}
     </main>
   );
 };
