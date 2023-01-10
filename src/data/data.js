c7d2e1683a8d5d814cda9fb6a652909110df94b3
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2023-01-10 14:00:18 +0100

    added arrays with images for AboutUs page, updated names

--- src/data/data.js
+++ src/data/data.js
@@ -1,36 +1,51 @@
-import image1 from "../assets/imagesSlider/7031408.jpg";
-import image2 from "../assets/imagesSlider/5502227.jpg";
-import image3 from "../assets/imagesSlider/5524205.jpg";
-import image4 from "../assets/imagesSlider/280222.jpg";
+import image0_mainSlider from "../assets/images/imagesSlider/7031408.jpg";
+import image1_mainSlider from "../assets//images/imagesSlider/5502227.jpg";
+import image2_mainSlider from "../assets/images/imagesSlider/5524205.jpg";
+import image3_mainSlider from "../assets/images/imagesSlider/280222.jpg";
+
+import image0_aboutUsDesktop from "../assets/images/aboutUs/934591.jpg";
+import image1_aboutUsDesktop from "../assets/images/aboutUs/6899397.jpg";
+import image2_aboutUsDesktop from "../assets/images/aboutUs/1928739.jpg";
+import image3_aboutUsDesktop from "../assets/images/aboutUs/6794920.jpg";
+import image4_aboutUsDesktop from "../assets/images/aboutUs/5823879.jpg";
+
+import image0_aboutUsMobile from "../assets/images/aboutUs/4065191.jpg";
+import image1_aboutUsMobile from "../assets/images/aboutUs/934591.jpg";
+import image2_aboutUsMobile from "../assets/images/aboutUs/6899397.jpg";
+import image3_aboutUsMobile from "../assets/images/aboutUs/1928739.jpg";
+import image4_aboutUsMobile from "../assets/images/aboutUs/6794920.jpg";
+import image5_aboutUsMobile from "../assets/images/aboutUs/5823879.jpg";
 
 // ARRAY OF OFFERS
 const offers = [
   {
     id: 0,
-    offerNumber: "",
-    propertyTypePL: "",
-    propertyTypeEN: "",
-    transactionTypePL: "",
-    transactionTypeEN: "",
-    marketTypePL: "",
-    marketTypeEN: "",
-    provincePL: "",
-    provinceEN: "",
-    place: "",
-    district: "",
-    price: "",
-    roomsNumber: "",
-    floorNumber: "",
-    numberOfFloors: "",
-    livingArea: "",
-    buildYear: "",
-    descriptionPL1: "",
-    descriptionEN1: "",
+    offerNumber: 245363,
+    propertyTypePL: "Apartment",
+    propertyTypeEN: "Mieszkanie",
+    transactionTypePL: "Sale",
+    transactionTypeEN: "Sprzedaż",
+    marketTypePL: "Primary",
+    marketTypeEN: "Pierwotny",
+    provincePL: "Dolnośląsie",
+    provinceEN: "Lower Silesia",
+    place: "Wrocław",
+    district: "Gaj",
+    price: 300000,
+    roomsNumber: 3,
+    floorNumber: 3,
+    numberOfFloors: 10,
+    livingArea: 50,
+    buildYear: 1980,
+    descriptionPL1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat mattis dui, eget luctus sem eleifend et. Integer pharetra ligula et fermentum cursus. Vivamus posuere venenatis condimentum. Ut ac eros sit amet urna ullamcorper pulvinar ut vel elit. Proin rhoncus tincidunt orci id placerat. Vestibulum porttitor eleifend leo, eu facilisis augue mollis vitae. Curabitur lacinia fermentum lorem, vitae auctor mauris tristique nec. Phasellus nisl sem, tincidunt eu feugiat at, molestie eu arcu. Vivamus bibendum sapien arcu, quis maximus arcu ornare a. Ut gravida tincidunt scelerisque.",
+    descriptionEN1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat mattis dui, eget luctus sem eleifend et. Integer pharetra ligula et fermentum cursus. Vivamus posuere venenatis condimentum. Ut ac eros sit amet urna ullamcorper pulvinar ut vel elit. Proin rhoncus tincidunt orci id placerat. Vestibulum porttitor eleifend leo, eu facilisis augue mollis vitae. Curabitur lacinia fermentum lorem, vitae auctor mauris tristique nec. Phasellus nisl sem, tincidunt eu feugiat at, molestie eu arcu. Vivamus bibendum sapien arcu, quis maximus arcu ornare a. Ut gravida tincidunt scelerisque.",
 
-    agentName: "",
-    agentSurname: "",
-    agentPhoneNumber: "",
-    agentEmailAddress: "",
+    agentName: "Place for Name",
+    agentSurname: "Place for Surname",
+    agentPhoneNumber: "place for phone number",
+    agentEmailAddress: "place for email address",
   },
 ];
 
@@ -38,7 +53,7 @@ const offers = [
 const offersImages = [
   {
     id: 0,
-    url: image1,
+    url: image0_mainSlider,
   },
 ];
 
@@ -46,7 +61,7 @@ const offersImages = [
 const autoMoveSliderData = [
   {
     id: 0,
-    url: image1,
+    url: image0_mainSlider,
     textLeft_EN: "PLACE FOR TEXT",
     textRight_EN: "PLACE FOR TEXT",
     textLeft_PL: "MIEJSCE NA TEKST",
@@ -54,7 +69,7 @@ const autoMoveSliderData = [
   },
   {
     id: 1,
-    url: image2,
+    url: image1_mainSlider,
     textLeft_EN: "PLACE FOR TEXT",
     textRight_EN: "PLACE FOR TEXT",
     textLeft_PL: "MIEJSCE NA TEKST",
@@ -62,7 +77,7 @@ const autoMoveSliderData = [
   },
   {
     id: 2,
-    url: image3,
+    url: image2_mainSlider,
     textLeft_EN: "PLACE FOR TEXT",
     textRight_EN: "PLACE FOR TEXT",
     textLeft_PL: "MIEJSCE NA TEKST",
@@ -70,7 +85,7 @@ const autoMoveSliderData = [
   },
   {
     id: 3,
-    url: image4,
+    url: image3_mainSlider,
     textLeft_EN: "PLACE FOR TEXT",
     textRight_EN: "PLACE FOR TEXT",
     textLeft_PL: "MIEJSCE NA TEKST",
@@ -78,4 +93,98 @@ const autoMoveSliderData = [
   },
 ];
 
-export { offers, offersImages, autoMoveSliderData };
+// ARRAY OF DATA FOR MAIN ABOUT US DESKTOP PAGE SLIDER - IMAGES
+const aboutUsSliderDataDesktop = [
+  {
+    id: 0,
+    url: image0_aboutUsDesktop,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+  {
+    id: 1,
+    url: image1_aboutUsDesktop,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+  {
+    id: 2,
+    url: image2_aboutUsDesktop,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+  {
+    id: 3,
+    url: image3_aboutUsDesktop,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+  {
+    id: 4,
+    url: image4_aboutUsDesktop,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+];
+// ARRAY OF DATA FOR MAIN ABOUT US MOBILE PAGE SLIDER - IMAGES
+const aboutUsSliderDataMobile = [
+  {
+    id: 0,
+    url: image0_aboutUsMobile,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+  {
+    id: 1,
+    url: image1_aboutUsMobile,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+  {
+    id: 2,
+    url: image2_aboutUsMobile,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+  {
+    id: 3,
+    url: image3_aboutUsMobile,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+  {
+    id: 4,
+    url: image4_aboutUsMobile,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+  {
+    id: 5,
+    url: image5_aboutUsMobile,
+    textLeft_EN: "",
+    textRight_EN: "",
+    textLeft_PL: "",
+    textRight_PL: "",
+  },
+];
+export { offers, offersImages, autoMoveSliderData, aboutUsSliderDataMobile, aboutUsSliderDataDesktop };
