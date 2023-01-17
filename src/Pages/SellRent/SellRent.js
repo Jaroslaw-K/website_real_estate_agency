a14f3e228f07c78726ec00c6377d87111eb99245
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2023-01-17 18:51:47 +0100

    added SellRentMobile and SellRentDesktop components

---
+++ src/Pages/SellRent/SellRent.js
@@ -0,0 +1,27 @@
+import "./SellRent.scss";
+import { BrowserView, MobileView } from "react-device-detect";
+// COMPONENTS
+import Slider from "../../Components/Slider/Slider";
+import SellRentDesktop from "./SellRentDesktop/SellRentDesktop";
+import SellRentMobile from "./SellRentMobile/SellRentMobile";
+// DATA FOR SLIDER
+import { autoMoveSliderData } from "../../data/data";
+
+const Contact = () => {
+  const moveSlider = (moveSlider) => {
+    moveSlider(); // FUNCTION RESPONSIBLE FOR AUTO-MOVE SLIDER
+  };
+  return (
+    <main className="contact">
+      <Slider moveSlider={moveSlider} slidesArray={autoMoveSliderData} />
+      <MobileView>
+        <SellRentMobile />
+      </MobileView>
+      <BrowserView>
+        <SellRentDesktop />
+      </BrowserView>
+    </main>
+  );
+};
+
+export default Contact;
