4e5267a851eedb38df0635f55e71c02989bcc679
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2023-01-20 15:39:28 +0100

    Added breakpoints variables

---
+++ src/data/variables.js
@@ -0,0 +1,8 @@
+// BREAKPOINTS
+const breaktPoint_0 = 0;
+const breaktPoint_600 = 600;
+const breaktPoint_900 = 900;
+const breaktPoint_1200 = 1200;
+const breaktPoint_1400 = 1400;
+
+export { breaktPoint_0, breaktPoint_600, breaktPoint_900, breaktPoint_1200, breaktPoint_1400 };
