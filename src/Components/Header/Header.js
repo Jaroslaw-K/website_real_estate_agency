a3a83eff333e10c15f6539b619877b16d2952b1a
Jarosław Karczewski <jaroslaw.karczewski.1986@gmail.com>
2023-01-20 16:29:56 +0100

    imported breakpoint variable

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import { Fragment, useState } from "react";
 import { useMediaQuery } from "react-responsive";
-
+import { breaktPoint_1200 } from "../../data/variables";
+// COMPONENTS
 import HeaderMobile from "./HeaderMobile/HeaderMobile";
 import HeaderDesktop from "./HeaderDesktop/HeaderDesktop";
 
@@ -8,16 +9,18 @@ const Header = () => {
   const [showNav, setShowNav] = useState(false);
   const [showLanguagesList, setShowLanguagesList] = useState(false);
 
-  const toggleNavHandler = () => { // RESPONSIBLE FOR SHOWING/HIDING MOBILE NAVIGATION
+  const toggleNavHandler = () => {
+    // RESPONSIBLE FOR SHOWING/HIDING MOBILE NAVIGATION
     setShowNav((previousState) => !previousState);
   };
-  const toggleLanguagesList = () => { // REPONSIBLE FOR DISPLAYING LANGUAGES LIST
+  const toggleLanguagesList = () => {
+    // REPONSIBLE FOR DISPLAYING LANGUAGES LIST
     setShowLanguagesList((previousState) => !previousState);
   };
   return (
     <Fragment>
-      {useMediaQuery({ maxWidth: 1200 }) && <HeaderMobile onToggleLanguagesList={toggleLanguagesList} showLanguagesList={showLanguagesList} onToggleNavHandler={toggleNavHandler} showNav={showNav} />}
-      {!useMediaQuery({ maxWidth: 1200 }) && <HeaderDesktop onToggleLanguagesList={toggleLanguagesList} showLanguagesList={showLanguagesList} />}
+      {useMediaQuery({ maxWidth: breaktPoint_1200 }) && <HeaderMobile onToggleLanguagesList={toggleLanguagesList} showLanguagesList={showLanguagesList} onToggleNavHandler={toggleNavHandler} showNav={showNav} />}
+      {!useMediaQuery({ maxWidth: breaktPoint_1200 }) && <HeaderDesktop onToggleLanguagesList={toggleLanguagesList} showLanguagesList={showLanguagesList} />}
     </Fragment>
   );
 };
